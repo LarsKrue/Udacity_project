c6231ff7b63afb205580bff778ad5f7068542bb1
LarsKrue <65137791+LarsKrue@users.noreply.github.com>
2020-06-02 22:27:51 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,67 +3,36 @@
 Created on Sat May 30 00:28:12 2020
 
 - infer a category from an image
-- pretrained model and specifically trained classifier
+- relaod pretrained model and specifically trained classifier
 
 @author: Lars Krüger
 """
-import matplotlib.pyplot as plt
+
 import torch as tr
-import os
-import json
+
+import image_functions as imf # own image functions
+import network_functions as nf # own network functions
 
 from get_input import get_predict_input # command Line arguments
-from torchvision import  transforms, models
-from torch import nn
-from collections import OrderedDict
-from skimage import io
-from PIL import Image
 
 
-def load_flower_cat_names(input_predict_args):  
-    #---------------------Load category names from .json file ----------------
-    # "cat_to_name.json" in directory flower_data
-    
-    dir_name = input_predict_args.dir
-    file_name = "cat_to_name.json"
-    file = os.path.join(dir_name, file_name)
-    with open(file, "r") as f:
-        cat_no_to_flower_name_dict = json.load(f)
-    #print("Network to be trained on {} categories".format(len(cat_no_to_flower_name_dict.keys())))
-    return(cat_no_to_flower_name_dict)
-
-def map_label_numbers_to_cat_names(label_list, cat_no_to_flower_name_dict):
-    # --------------------- Mapping cat numbers to flower names----------------
-    category_names = []
-    for label in label_list:
-        category_names = category_names.append(cat_no_to_flower_name_dict[str(label)]) # key in dict is string/ category is int
-    print("Liste: ", category_names)
-    return category_names
 
 
-#---------------get user input------------------------------------------------
-
+# get user input 
 input_predict_args = get_predict_input()
+configuration_path = input_predict_args.config
+state_dict_path = input_predict_args.state
 print(input_predict_args)
 
-#--------------- load pretrained NN and pretrained classifier-----------------
 
-configuration = tr.load("configuration_dict.pth")
-
-
-if configuration["architecture"] == "resnet18":
-       my_network = models.resnet18(pretrained=True)
-       print("Resnet18 pretrained model loaded")
-       input_units_nn = 512 # per definition
-else:
-       my_network = models.resnet50(pretrained=True)
-       print("Resnet50 pretrained model loaded")
-       input_units_nn = 2048 # per definition
-
-#-----------------------------Load pretrained own classifier ------------------
+# load pretrained NN-model
+configuration = tr.load(configuration_path)
+state_dict = tr.load(state_dict_path)
+my_reloaded_network = nf.reload_network_predict(configuration)
 
 
-# Overview over trained classifier
+# overview pretrained own classifier
+print("------------------------Inference-------------------------------------")
 print("No of input units: {}".format(configuration["input_units"]))
 print("No of hidden units: {}".format(configuration["hidden_units"]))
 print("Architecture of NN: {}".format(configuration["architecture"]))
@@ -71,91 +40,54 @@ print("Accuracy on validation data set: {}".format(configuration["accuracy"]))
 print("Epochs trained: {}".format(configuration["epochs"]))
 print("Trained on: {}".format(configuration["device"]))
 tot_time = configuration["total_time"]
-print("Time needed to train: ", str(int((tot_time/3600)))+"h:"+str(int((tot_time%3600)/60))+":min"
-      +str(int((tot_time%3600)%60))+":sek" )
+print("Time needed to train: ", str(int((tot_time/3600)))+"h:"+str(int((tot_time%3600)/60))+"min:"
+      +str(int((tot_time%3600)%60))+"sek" )
 
-#-- ensure compatibility with specifically created new classifier 
-# Design of new Classifer Network
-input_units = configuration["input_units"]
-hidden_units = configuration["hidden_units"]
-output_units = configuration["output_units"]
+# setup and re-load pretrained classifier
+my_reloaded_network = nf.reload_classifier(configuration,state_dict, my_reloaded_network)
 
-state_dict = tr.load("checkpoint_trained_network.pth")
-my_network.load_state_dict(state_dict, strict=False) # pytorch forum, otherwise missing & unexpected keys
+# PIL image Loading
+test_image = imf.img_load(input_predict_args)
 
+# image preprocessing PIL to Tensor
+img = imf.image_preprocess(input_predict_args)
 
-classifier = nn.Sequential(OrderedDict([
-                          ("Layer 1", nn.Linear(input_units, hidden_units)),
-                          ("ReLu", nn.ReLU()),
-                          ("Layer 2", nn.Linear(hidden_units, output_units)),
-                          ("Output", nn.LogSoftmax(dim=1))
-                          ]))
+# inference and top5 results
+my_reloaded_network.eval() # inference mode
+k=input_predict_args.k #k-top results from input parser / CL
 
-#Assignment of new classifier structure to network, but yet untrained
-my_network.fc = classifier
+device = nf.check_cuda_availability(input_predict_args)
 
-#---------------------Image Loading-------------------------------------------
-
-file_name =input_predict_args.file
-
-test_image = io.imread(file_name)
-#test_image = test_image[250:850, 250:650] # manual center crop
-plt.imshow(test_image) # all channels, real image
-
-print(test_image.size)
-print(test_image.shape) #3 channel image, RGB
-
-#--------------------Image preprocessing--------------------------------------
-
-size = 250
-crop = 224
-single_transform = transforms.Compose([transforms.Resize((size, size)),# returns PIL image
-                                       transforms.CenterCrop(crop),
-                                       transforms.ToTensor(), # returns tensor
-                                       transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]), # expects tensor                                
-                                       ])
-img = Image.open(file_name)
-img = single_transform(img) # 
-img = img.unsqueeze(0)
-
-#--------------------------Inference------------------------------------------
-my_network.eval() # inference mode
-k=5 #k-top results
 with tr.no_grad(): # no gradient calculation
-    logps = my_network.forward(img)
+    my_reloaded_network.to(device)
+    img=img.to(device)
+    logps = my_reloaded_network.forward(img)
     ps = tr.exp(logps)
-    print("ps", ps)
-    top_p, top_class = ps.topk(k, dim=1)
-    print("Top p", top_p)
-    print("Top Class", top_class)
-    print("Tuples:", zip(top_p, top_class))
- 
+    top_p, top_class = ps.topk(k, dim=1, sorted=True)
 
+if device == "cuda:0":
+    img = img.cpu()
+    my_reloaded_network = my_reloaded_network.to("cpu")
 
-#----------------------------Map label numbers to category names--------------
+# prepare inference results for label name matching
 top_label_list = top_class.tolist()[0] # tensor to list, first element got get dim 1 list
 top_p_list = top_p.tolist()[0] # tensor to list, first element got get dim 1 list
-top_p_list = [100 * p for p in top_p_list]
-print("Top Class:",top_label_list)
-print("Top prob:",top_p_list)
+top_p_list = [100 * p for p in top_p_list] # get percent values (%)
 
-# load json file
-cat_no_to_flower_name_dict = load_flower_cat_names(input_predict_args)
-# mapp category names to top5 labels
-category_names_list = [cat_no_to_flower_name_dict[str(label)] for label in top_label_list ]
-print(category_names_list)
+# load json file, create dict
+cat_no_to_flower_name_dict = imf.load_flower_cat_names(input_predict_args)
 
+# map category names to top5 labels
+category_names_list = [cat_no_to_flower_name_dict[str(label)] for label in top_label_list]
 
-#-----------------------------Plot img + Category Probability------------------
+# combination of top-k results category number and flower name as result
+result_list=[str((names, labels)) for names, labels in zip(top_label_list,category_names_list)]
 
-fig, (ax1, ax2, ax3) = plt.subplots(figsize=(12,6), ncols=3)
-ax1.barh([1,2,3,4,5],top_p_list,0.6,tick_label=category_names_list)
+print("\n----------------------Prediction results-----------------------------\n")
+for i in range(k): 
+    print("Pos. {}: Category: {} - Probability: {}% ".format(i+1, result_list[i], round(top_p_list[i],2)))
+  
+    
+# plot img + Category names & probability
+imf.plot_prediction(configuration,input_predict_args, result_list, top_p_list, test_image, img)
 
-ax1.set_aspect("auto")
-ax1.set_title('Class Probability')
-ax2.imshow(img.resize_(1, 224, 224).numpy().squeeze(), aspect=1)
-ax2.axis('on')
-ax2.set_title(file_name + " - processed") 
-ax3.imshow(test_image)
-ax3.axis('on')
-ax3.set_title(file_name + " - real") 
